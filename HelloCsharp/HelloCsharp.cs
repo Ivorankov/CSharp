05649e429d36fb4b0d9bee3d4b919d63b2ee47e1
Ivorankov <ivo_rankov@mail.bg>
2015-01-08 21:16:26 +0200

    Homework

---
+++ HelloCsharp/HelloCsharp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Create, compile and run a “Hello C#” console application.
+namespace HelloCsharp
+{
+    class HelloCsharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.ReadLine();
+        }
+    }
+}
