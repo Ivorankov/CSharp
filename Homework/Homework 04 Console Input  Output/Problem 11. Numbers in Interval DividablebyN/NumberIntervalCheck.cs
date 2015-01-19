25c4e6634bc27e167e6a1863f98e204a46bcb9c2
Ivorankov <ivo_rankov@mail.bg>
2015-01-19 22:07:35 +0200

    Update NumberIntervalCheck.cs

--- Homework/Homework 04 Console Input  Output/Problem 11. Numbers in Interval DividablebyN/NumberIntervalCheck.cs
+++ Homework/Homework 04 Console Input  Output/Problem 11. Numbers in Interval DividablebyN/NumberIntervalCheck.cs
@@ -11,13 +11,13 @@ namespace Problem_11.Numbers_in_Interval_DividablebyN
     {
         static void Main(string[] args)
         {
-            int begin, end, i;
+            int begin, end,count, i;
             List<int> listNumbers = new List<int>();
             string numbers;
 
             Console.WriteLine("This program will do something...not sure what but...");
             Console.WriteLine();
-            Console.WriteLine("Ok Ok you choose two positive numbers and then the program will tell you which numbers in  between will be devided by 5 will = 0");
+            Console.WriteLine("Ok Ok you choose two positive numbers and then the program will tell you which numbers in  between when devided by 5 will = 0");
             Console.WriteLine();
             Console.Write("Please enter the starting number: ");
             while(!int.TryParse(Console.ReadLine(), out begin))                          //This part will validate the user input.
@@ -29,17 +29,18 @@ namespace Problem_11.Numbers_in_Interval_DividablebyN
             {
                 Console.WriteLine("Please use numeric values");
             }
-
+            count = 0;
             for (i = begin;i <= end;i++)                                                 //This will start a loop from the starting to the ending number
             {
                 if(i % 5 == 0)                                                           //If any of the numbers devided by 5 = 0, it will add them to a list
                 {
                     listNumbers.Add(i);
+                    count++;
                 }
             }
             Console.WriteLine();
             numbers = string.Join(", ", listNumbers.ToArray());                          //This will put the numbers in the list in a string 
-            Console.WriteLine("The numbers are: " + numbers);                            //This will print the numbers to the console
+            Console.WriteLine("The numbers are: " + numbers + " | " + count + " in total."); //This will print the numbers to the console
         }
     }
 }
