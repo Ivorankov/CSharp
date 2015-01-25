97f5e54735e2c03db0ae7bf476c3158b55ec929b
Ivorankov <ivo_rankov@mail.bg>
2015-01-25 19:08:55 +0200

    Temp

---
+++ Temp/ConsoleApplication3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Nightmare_on_Code_Street
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum,final;
+            string user = Console.ReadLine();
+            char[] tester = user.ToCharArray();
+            sum = 0;
+            int counter = 0;
+            string nov = string.Empty;
+            for (int x = 1; x < tester.Length; x = x + 2)
+            {
+                if (Char.IsDigit(tester[x]))
+                {
+                    nov = nov + tester[x];
+                    counter++;
+                }
+            }
+            long.TryParse(nov, out final);
+            for (int y = 0; y <= nov.Length; y++)
+            {
+                sum = sum + final % 10;
+                final /= 10;
+            }
+            Console.WriteLine(counter + " " + sum);
+        }
+    }
+}
